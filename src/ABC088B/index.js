6a35f52f5e3c36e2e168d36e99cbaadc6452ebd5
t29mato <tomoya.matou@gmail.com>
2020-03-29 06:05:54 +0900

    sort関数利用

--- src/ABC088B/index.js
+++ src/ABC088B/index.js
@@ -1,18 +1,11 @@
 const Main = (input) => {
     const N = input.split('\n')[0]
     const a = input.split('\n')[1].split(' ')
-    const numbers = a.map(str => parseInt(str))
-    array = []
-
-    for (i = 0; i < N; i++) {
-        max = Math.max(...numbers)
-        array.push(max)
-        numbers.splice(numbers.indexOf(max), 1)
-    }
+    const numbers = a.map(str => parseInt(str)).sort((a, b) => b - a)
 
     alice = 0;
     bob = 0;
-    array.reduce((prev, current, index) => {
+    numbers.reduce((prev, current, index) => {
         if (index % 2) {
             bob += current
         } else {
