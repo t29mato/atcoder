7a6de1272b96fb33d49170394d24a8b8d9161733
t29mato <tomoya.matou@gmail.com>
2020-03-29 07:37:20 +0900

    reduceやめたら万事解決。うーん、謎が残る

--- abs/abc085b/index.js
+++ abs/abc085b/index.js
@@ -2,18 +2,13 @@ const Main = (input) => {
     tmp = input.split('\n')
     numbers = tmp.map(n => parseInt(n))
     numbers.shift()
-    numbers.sort((a, b) => b - a)
-    count = 0
-    numbers.reduce((prev, current) => {
-        if (prev == current) {
-            return prev
-        } else {
-            count++
-            return current
+    array = []
+    numbers.forEach(n => {
+        if (array.indexOf(n) == -1) {
+            array.push(n)
         }
-    })
-    console.log(count)
-    return count
+    });
+    console.log(array.length - 1)
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、mainを呼び出します）
