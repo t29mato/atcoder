362f3ef5f52f1757e31e9c07fbf660e15b71ebc0
t29mato <tomoya.matou@gmail.com>
2020-03-29 01:59:36 +0900

    case closed

---
+++ src/ABC081A/index.js
@@ -0,0 +1,12 @@
+const main = (input) => {
+    const array = input.split('')
+    const count = array.reduce((a, c, i) => {
+        if (c == 1) {
+            a += 1
+        }
+        return a
+    }, 0)
+    return count
+}
+
+module.exports = main
