771d5bfdc8877f669a6ced86da3c5649f68f4f53
t29mato <tomoya.matou@gmail.com>
2020-03-29 21:38:54 +0900

    ようやくできた!

--- abs/abc086c/index.js
+++ abs/abc086c/index.js
@@ -1,26 +1,21 @@
-const Main = (input) => {
-    data = input.split('\n')
-    const N = data[0]
-    data.shift()
-    data.pop()
-    const initial = [0, 0, 0]
-    array = data.map((item) => item.split(' ').map(i => parseInt(i)))
+const Main = input => {
+    data = input.slice(1, +input[0] + 1)
+    numbers = data.map(item => item.split(' ').map(i => parseInt(i)))
     result = 'NO'
-    const calculate = array.reduce((prev, cur) => {
+    numbers.reduce((prev, cur) => {
         time = cur[0] - prev[0]
-        timeX = cur[1] - prev[1]
-        timeY = cur[2] - prev[2]
+        timeX = Math.abs(cur[1] - prev[1])
+        timeY = Math.abs(cur[2] - prev[2])
         timeSum = timeX + timeY
-        // console.log(time, timeX, timeY);
-        if (timeSum <= time && (timeSum - time) %2 === 0) {
+        if (timeSum <= time && timeSum % 2 === time % 2) {
             result = 'Yes'
         } else {
             result = 'No'
         }
-        return [time, timeX, timeY]
-    }, initial)
+        return [cur[0], cur[1], cur[2]]
+    }, [0, 0, 0])
     console.log(result)
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、mainを呼び出します）
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+Main(require("fs").readFileSync("/dev/stdin", "utf8").split('\n'));
