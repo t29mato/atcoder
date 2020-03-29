c5be265f2fe540a0649d02159728d3dc11f62b7c
t29mato <tomoya.matou@gmail.com>
2020-03-29 16:55:54 +0900

    正規表現バージョンに変更

--- abs/abc049c/index.js
+++ abs/abc049c/index.js
@@ -1,35 +1,11 @@
 const Main = (input) => {
-    reverseMessage = input.split('\n')[0].split('').reverse().join('')
-    reverseWord = ['dream', 'dreamer', 'erase', 'eraser'].map(word => word.split('').reverse().join(''))
-    count = 0
-    while(count < 4) {
-        count = 0
-        reverseWord.forEach(word => {
-            if (reverseMessage.indexOf(word) === 0) {
-                reverseMessage =　reverseMessage.replace(word, '')
-            } else {
-                count++
-            }
-        });
-    }
-    if (reverseMessage.length === 0) {
+    const data = input.split('\n')[0]
+    const regex = /^(erase|eraser|dream|dreamer)*$/g;
+    if (data.match(regex)) {
         console.log('YES')
     } else {
         console.log('NO');
     }
-}
-
-const recursion = (text) => {
-    if (text.length === 0) {
-        return 'YES'
-    }
-    const sleeping = ['dream', 'dreamer', 'erase', 'eraser']
-
-    sleeping.map((s) => {
-        idx = text.indexOf(s)
-        trimText = text.replace(s, '')
-        recursion(trimText)
-    })
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、mainを呼び出します）
