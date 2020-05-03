7f0501257198dde4f80e9b91debb555cbfd55102
t29mato <tomoya.matou@gmail.com>
2020-05-03 23:14:58 +0900

    refactor: main関数削除

--- abc166/b/index.js
+++ abc166/b/index.js
@@ -1,32 +1,23 @@
-const Main = (input) => {
-    d = []
-    A = []
-    array = input.split('\n')
-    NK = array.shift()
-    array.pop()
-    dA = array.map(item => item.split(' ').map(value => parseInt(value)))
-    NK = NK.split(' ').map(item => parseInt(item))
-    for (i = 0; i < NK[1]; i++) {
-        d.push(dA.shift())
-        A.push(dA.shift())
-    }
-    console.log(F(NK[0], NK[1], d, A))
+const input = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+
+const NK = input[0].split(' ').map(item => parseInt(item, 10))
+const N = NK[0]
+const K = NK[1]
+let d = []
+let A = []
+input.shift()
+input.pop()
+dA = input.map(item => item.split(' ').map(item => parseInt(item, 10)))
+for (i = 0; i < NK[1]; i++) {
+    d.push(dA.shift())
+    A.push(dA.shift())
 }
 
-const F = (N, K, d, A) => {
-    // console.log('N: ', N)
-    // console.log('K: ', K)
-    // console.log('d: ', d)
-    // console.log('A: ', A)
-    nosuke = new Map()
-    for (i = 0; i < K; i++) {
-        for (k = 0; k < d[i]; k++) {
-            nosuke.set(A[i][k], true)
-        }
+nosuke = new Map()
+for (i = 0; i < K; i++) {
+    for (k = 0; k < d[i]; k++) {
+        nosuke.set(A[i][k], true)
     }
-    result = N - nosuke.size
-    return result
 }
-
-//*この行以降は編集しないでください（標準入出力から一度に読み込み、mainを呼び出します）
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+result = N - nosuke.size
+console.log(result)
