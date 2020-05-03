dafdc4de3eef8f7e3cc1a636abcd840ead0b7487
t29mato <tomoya.matou@gmail.com>
2020-05-03 23:18:17 +0900

    refactor

--- abc166/b/index.js
+++ abc166/b/index.js
@@ -8,16 +8,14 @@ let A = []
 input.shift()
 input.pop()
 dA = input.map(item => item.split(' ').map(item => parseInt(item, 10)))
+nosuke = new Map()
 for (i = 0; i < K; i++) {
     d.push(dA.shift())
     A.push(dA.shift())
-}
-
-nosuke = new Map()
-for (i = 0; i < K; i++) {
     for (k = 0; k < d[i]; k++) {
         nosuke.set(A[i][k], true)
     }
 }
+
 result = N - nosuke.size
 console.log(result)
