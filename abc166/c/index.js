a0057bff31ce8b14086b726862367bba210e7f20
t29mato <tomoya.matou@gmail.com>
2020-05-03 23:42:22 +0900

    refactor: indent１つ削除

--- abc166/c/index.js
+++ abc166/c/index.js
@@ -1,40 +1,29 @@
-const Main = (input) => {
-    array = input.split('\n')
-    NM = array.shift().split(' ').map(item => parseInt(item))
-    H = array.shift().split(' ').map(item => parseInt(item))
-    array.pop() // lastが邪魔
-    A = array.map(item => item.split(' ').map(value => parseInt(value)))
-    // console.log('NM: ', NM)
-    // console.log('H: ', H)
-    // console.log('A: ', A)
-    console.log(F(NM[0], NM[1], H, A))
-}
+let input = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
+const NM = input.shift().split(' ').map(item => parseInt(item))
+const N = NM[0]
+const M = NM[1]
+const H = input.shift().split(' ').map(item => parseInt(item))
+input.pop() // lastが邪魔
+const A = input.map(item => item.split(' ').map(value => parseInt(value)))
 
-const F = (N, M, H, A) => {
-    road = []
-    for (i = 0; i <= N; i++) {
-        road.push([])
-    }
-    for (i = 0; i < M; i++) {
-        road[A[i][0]].push(A[i][1])
-        road[A[i][1]].push(A[i][0])
-    }
-    count = 0
-    // console.log('road: ', road)
-    for (i = 1; i <= N; i++) {
-        // console.log('i: ', i, road[i])
-        if (road[i].length == 0) {
-            count++
-            continue
-        }
-        max = 0
-        road[i].forEach(item => {
-            if (H[item - 1] > max) max = H[item - 1]
-        });
-        if (max < H[i - 1]) count++
-    }
-    return count
+let road = []
+for (i = 0; i <= N; i++) {
+    road.push([])
 }
-
-//*この行以降は編集しないでください（標準入出力から一度に読み込み、mainを呼び出します）
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+for (i = 0; i < M; i++) {
+    road[A[i][0]].push(A[i][1])
+    road[A[i][1]].push(A[i][0])
+}
+let count = 0
+for (i = 1; i <= N; i++) {
+    if (road[i].length == 0) {
+        count++
+        continue
+    }
+    max = 0
+    road[i].forEach(item => {
+        if (H[item - 1] > max) max = H[item - 1]
+    });
+    if (max < H[i - 1]) count++
+}
+console.log(count)
