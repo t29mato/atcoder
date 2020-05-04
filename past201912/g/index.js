f61012a9a4cc59565883e327d73dbc7621ef794e
t29mato <tomoya.matou@gmail.com>
2020-05-05 00:02:27 +0900

    ケース修正

--- past201912/g/index.js
+++ past201912/g/index.js
@@ -2,16 +2,8 @@ const input = require("fs").readFileSync("/dev/stdin", "utf8").split("\n");
 
 const firstLine = input[0].split(' ').map(item => parseInt(item))
 const N = firstLine[0]
-const Y = firstLine[1]
 
-result = '-1 -1 -1'
-for (i = 0; i <= N; i++) {
-    for (j = 0; j <= N - i; j++) {
-        for (k = N - (i + j); k <= N - (i + j); k++) {
-            if (10000 * i + 5000 * j + 1000 * k === Y) {
-                result = `${i} ${j} ${k}`
-            }
-        }
-    }
-}
-console.log(result)
+input.shift()
+
+A = input
+console.log(A)
