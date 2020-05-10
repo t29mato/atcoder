eb7249e49e7534886a56f1b5a6c1b3a451cf867e
t29mato <tomoya.matou@gmail.com>
2020-05-10 20:16:30 +0900

    JSでの再帰関数の上限は1万ちょっとなのに対して今回N最大10万だったためforループで再実装でAC

--- dp/a/index.js
+++ dp/a/index.js
@@ -5,16 +5,10 @@ const H = input[1].split(' ').map(item => parseInt(item))
 
 dp = new Array()
 // console.log(dp)
-
-const dfs = (pos) => {
-    // console.log(pos, dp)
-    if (dp[pos]) return dp[pos]
-    if (pos == 0) return dp[pos] = 0
-    if (pos == 1) return dp[pos] = dfs(pos-1) + Math.abs(H[pos] - H[pos-1])
-    step1 = dfs(pos-1) + Math.abs(H[pos] - H[pos-1])
-    step2 = dfs(pos-2) + Math.abs(H[pos] - H[pos-2])
-    return dp[pos] = Math.min(step1, step2)
+dp[0] = 0
+dp[1] = Math.abs(H[1] - H[0])
+for (i=2; i<N; i++) {
+    dp[i] = Math.min(dp[i-1] + Math.abs(H[i]-H[i-1]), dp[i-2] + Math.abs(H[i]-H[i-2]))
 }
 
-console.log(dfs(N-1))
-// console.log(dp)
+console.log(dp[N-1])
