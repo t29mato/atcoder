7d528aa7e209ea871fe775b9a79120f2f4fc6bd7
t29mato <tomoya.matou@gmail.com>
2020-05-17 21:14:07 +0900

    fix directory

---
+++ abc/168/a/index.js
@@ -0,0 +1,9 @@
+const input = require("fs").readFileSync("/dev/stdin", "utf8")
+let cin = input.split(/ |\n/), cid = 0
+const next = () => cin[cid++]
+const nexts = (n) => cin.slice(cid, cid+=n).map(i=>parseInt(i))
+
+const K = nexts(1)
+const S = nexts(1)
+
+console.log(K, S)
