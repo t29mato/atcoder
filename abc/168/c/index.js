77c9a2cdd5c7041a4792a4b25c07a46ab4ee77cc
t29mato <tomoya.matou@gmail.com>
2020-05-17 22:16:42 +0900

    C問題 初AC ><

--- abc/168/c/index.js
+++ abc/168/c/index.js
@@ -7,8 +7,10 @@ const [A, B, H, M] = nexts(4)
 
 const long = M * 6.0
 const short = (H + M / 60) * 30.0
-const angle = Math.abs(long - short)
+let angle = Math.abs(long - short)
 
 // console.log(long, short, angle)
-const result = Math.sqrt(A**2 + B**2 - 2 * 3 * 4 * Math.cos(angle * (Math.PI / 180)))
+
+// console.log(long, short, angle)
+const result = Math.sqrt(A**2 + B**2 - 2 * A * B * Math.cos(angle * (Math.PI / 180)))
 console.log(result)
