45daf8a234a0b8b98af276f9a9c74894dd1881ae
t29mato <tomoya.matou@gmail.com>
2020-06-13 21:05:53 +0900

    AC

---
+++ tokiomarine2020/a/index.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin", "utf8")
+let cin = input.split(/ |\n/), cid = 0
+const next = () => cin[cid++]
+const nexts = (n) => cin.slice(cid, cid+=n).map(i=>parseInt(i))
+
+const S = next()
+console.log(S.slice(0, 3))
