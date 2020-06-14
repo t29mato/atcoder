22763b6907f73b9e394a0c79f07eeb2693e1b914
t29mato <tomoya.matou@gmail.com>
2020-06-14 21:04:12 +0900

    AC

---
+++ abc/170/a/index.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin", "utf8")
+let cin = input.split(/ |\n/), cid = 0
+const next = () => cin[cid++]
+const nexts = (n) => cin.slice(cid, cid+=n).map(i=>parseInt(i))
+
+const numbers = nexts(5)
+console.log(numbers.indexOf(0) + 1)
